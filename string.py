e36afbc453a1d20370f5b6b4a59b65883926d351
root <hish.b92@gmail.com>
2020-04-07 16:42:17 +0500

    upper

---
+++ string.py
@@ -0,0 +1,10 @@
+# Create a method is_uppercase() to see whether the string is ALL CAPS. For example:
+
+def is_uppercase(inp):
+  if inp == inp.upper():
+    return True
+  else:  
+    return False
+
+
+print(is_uppercase("cAt"))

8d73366a82b89e63e809168643cb9501dfe7c246
root <hish.b92@gmail.com>
2020-04-07 18:53:40 +0500

    swap

---
+++ swap.py
@@ -0,0 +1,8 @@
+# Define String.prototype.toAlternatingCase 
+# (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details)
+#  such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase
+
+def to_alternating_case(string):
+  return string.swapcase()
+
+print(to_alternating_case("hI"))  
